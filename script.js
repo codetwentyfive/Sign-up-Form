7bdfa7e37ce8f2bc1de5564c8ad696a992709ae4
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-07-05 16:59:50 +0200

    added js script link

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const password1= document.getElementById('password1').value;
-const password2= document.getElementById('password2').value;
+function validateForm() {
+
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+
+    if (password1 !== password2) {
+        alert("Passwords dont match");
+        return false;
+    }
+    
+    return true;
+}
